2daa64c2da9673a54a043bb692be3f07c9ff6637
ANTONIO MARCOS AMBROSIO MALAQUIAS <antonio_malaquias@estudante.sesisenai.org.br>
2025-12-02 20:13:48 -0300

    arrumando server.js

---
+++ src/app/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+// const db = require('/');
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(cors({origin:true,credentials:true}));
+
+app.listen(300,()=>{
+    console.log(`Rodando na porta: ${port}`)
+});
