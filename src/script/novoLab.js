8d03b27b0abf88ecf7a4f0b40164fadfc13a1f03
Antonio Malaquias <antoniomarcosmalaquias@hotmail.com>
2025-12-04 15:37:09 -0300

    começando o script do novoLab

---
+++ src/script/novoLab.js
@@ -0,0 +1,28 @@
+let session = localStorage.getItem('dados.session');
+let noma = localStorage.getItem('dados.nome');
+const nome = document.getElementById('name');
+const inputsCadastro = document.getElementsByClassName('inputsCadastro')
+const buttonAddLab = document.getElementById('buttonAddLab');
+
+
+buttonAddLab.addEventListener('click',()=>{
+    let labInfo = [];
+    for(let x = 0; x < inputsCadastro.length; x++){
+        if(!inputsCadastro[x].value){
+            alert('falta informação');
+            break;
+        }
+        labInfo.push(inputsCadastro[x].value);
+    }
+    console.log(labInfo);
+})
+
+
+// localStorage.removeItem('dados.session');
+// localStorage.removeItem('dados.nome');
+// session = null;
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    nome.innerText = `${noma}`;
+})
